1ff9385402bbb2474d260c8505bfa0d95f0515b6
bin <359584204@qq.com>
2023-05-08 14:09:17 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), WindiCSS()],
+  base: './',
+  build: {
+    outDir: 'docs'
+  }
 })
